01db912ba6142438e9496dcc36c096b2d8c36f59
mrinxx <mrinxx5@gmail.com>
2020-11-26 16:40:13 +0100

    Primer commit: Solo se han cambiado .env y se han creado las migraciones con sus campos

---
+++ database/migrations/2020_11_26_152805_create_usuarios_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('rol',20);
+            $table->string('nombre',100);
+            $table->string('apellido',200);
+            $table->string('nombre_usuario',100)->unique();
+            $table->string('email',255)->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password',255);
+            $table->string('imagen',255);
+            $table->dateTime('fecha_creacion');
+            $table->dateTime('fecha_actualizacion');
+            // $table->rememberToken();
+            $table->rememberToken('recordar_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
---
+++ database/migrations/2020_11_26_152932_create_imagenes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagenesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagenes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_usuario')->constrained('usuarios');
+            $table->string('ruta_imagen',255);
+            $table->text('description');
+            $table->dateTime('fecha_creacion');
+            $table->dateTime('fecha_actualizacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagenes');
+    }
+}
